1212b95d496b3e26c5dc5998e59b4cfb9d17d9e8
torcoroma <torcoromaarias@gmail.com>
2023-05-05 15:18:41 -0500

    errores corregdo

--- js/index.js
+++ js/index.js
@@ -1,56 +1,150 @@
-const inputTitulo = document.getElementById("titulo");
-const textarea = document.getElementById("descripcion");
-const inputFecha = document.getElementById("fecha");
-const formulario = document.getElementById("formulario-tarea");
-const listPendiente = document.getElementById("lista-pendientes");
-const listaProceso = document.getElementById("lista-progreso");
-const listaCompleta = document.getElementById("lista-completadas");
+// Obtener elementos del DOM
+const newTaskForm = document.querySelector(".new-task form");
+const pendingTasksList = document.getElementById("pending-tasks");
+const inProgressTasksList = document.getElementById("in-progress-tasks");
+const completedTasksList = document.getElementById("completed-tasks");
 
-let notas = JSON.parse(localStorage.getItem("notas")) || {
-  pendiente: [],
-  progreso: [],
-  completas: [],
+// Obtener tareas almacenadas en localStorage
+let tasks = JSON.parse(localStorage.getItem("tasks")) || {
+  pending: [],
+  inProgress: [],
+  completed: [],
 };
-console.log(notas);
-function crearlocalStorage() {
-  notas.pendiente.forEach((tarea) => {
-    const tareaElement = createTaskElement(tarea);
-    listPendiente.appendChild(tareaElement);
+
+// Función para actualizar las tareas almacenadas en localStorage
+function updateTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// Función para agregar una tarea a la lista de tareas pendientes
+function addTask(title, description, dueDate) {
+  const task = {
+    title,
+    description,
+    dueDate,
+    status: "pending",
+  };
+  tasks.pending.push(task);
+  updateTasks();
+  renderTasks();
+}
+
+// Función para editar una tarea existente
+function editTask(id, title, description, dueDate, status) {
+  const task = tasks[status].find((task) => task.id === id);
+  task.title = title;
+  task.description = description;
+  task.dueDate = dueDate;
+  if (task.status !== status) {
+    tasks[task.status] = tasks[task.status].filter((task) => task.id !== id);
+    task.status = status;
+    tasks[status].push(task);
+  }
+  updateTasks();
+  renderTasks();
+}
+
+// Función para eliminar una tarea
+function deleteTask(id, status) {
+  tasks[status] = tasks[status].filter((task) => task.id !== id);
+  updateTasks();
+  renderTasks();
+}
+
+// Función para renderizar las tareas en las diferentes listas
+// Función para renderizar las tareas en las diferentes listas
+function renderTasks() {
+  pendingTasksList.innerHTML = "";
+  inProgressTasksList.innerHTML = "";
+  completedTasksList.innerHTML = "";
+  tasks.pending.forEach((task) => {
+    const li = document.createElement("li");
+    li.classList.add("task");
+    li.dataset.id = task.id;
+    li.dataset.status = "pending";
+    li.innerHTML = `
+      <h3>${task.title}</h3>
+      <p>${task.description}</p>
+      <p>Fecha de vencimiento: ${task.dueDate}</p>
+      <div class="actions">
+        <button class="edit-btn">Editar</button>
+        <button class="delete-btn">Eliminar</button>
+      </div>
+    `;
+    pendingTasksList.appendChild(li);
   });
-  notas.progreso.forEach((tarea) => {
-    const tareaElement = createTaskElement(tarea);
-    listaProceso.appendChild(tareaElement);
+  tasks.inProgress.forEach((task) => {
+    const li = document.createElement("li");
+    li.classList.add("task", "in-progress");
+    li.dataset.id = task.id;
+    li.dataset.status = "inProgress";
+    li.innerHTML = `
+      <h3>${task.title}</h3>
+      <p>${task.description}</p>
+      <p>Fecha de vencimiento: ${task.dueDate}</p>
+      <div class="actions">
+        <button class="edit-btn">Editar</button>
+        <button class="delete-btn">Eliminar</button>
+      </div>
+    `;
+    inProgressTasksList.appendChild(li);
   });
-  notas.completas.forEach((tarea) => {
-    const tareaElement = createTaskElement(tarea);
-    listaCompleta.appendChild(tareaElement);
+  tasks.completed.forEach((task) => {
+    const li = document.createElement("li");
+    li.classList.add("task", "done");
+    li.dataset.id = task.id;
+    li.dataset.status = "completed";
+    li.innerHTML = `
+      <h3>${task.title}</h3>
+      <p>${task.description}</p>
+      <p>Fecha de vencimiento: ${task.dueDate}</p>
+      <div class="actions">
+        <button class="edit-btn">Editar</button>
+        <button class="delete-btn">Eliminar</button>
+      </div>
+    `;
+    completedTasksList.appendChild(li);
+  });
+
+  // Agregar eventos para editar y eliminar tareas
+  document.querySelectorAll(".task").forEach((task) => {
+    task.querySelector(".edit-btn").addEventListener("click", () => {
+      const form = document.querySelector(".edit-task form");
+      form.querySelector('input[name="title"]').value =
+        task.querySelector("h3").textContent;
+      form.querySelector('textarea[name="description"]').value =
+        task.querySelector("p:nth-of-type(1)").textContent;
+      form.querySelector('input[name="due-date"]').value = task
+        .querySelector("p:nth-of-type(2)")
+        .textContent.replace("Fecha de vencimiento: ", "");
+      form.querySelector(
+        'input[name="status"][value="' + task.dataset.status + '"]'
+      ).checked = true;
+      form.dataset.id = task.dataset.id;
+      form.dataset.status = task.dataset.status;
+      document.querySelector(".edit-task").classList.add("active");
+    });
+
+    task.querySelector(".delete-btn").addEventListener("click", () => {
+      const id = task.dataset.id;
+      const status = task.dataset.status;
+      deleteTask(id, status);
+    });
   });
 }
 
-crearlocalStorage();
-// En esta función se crea la card
-
-function crearLista() {
-  const titulo = inputTitulo.value;
-  const texto = textarea.value;
-  const fecha = inputFecha.value;
-  const card = `
-<div class="card">
- <div class="card-body">
-   <h5 class="card-title">${titulo}</h5>
-    <p class="card-text">${texto}</p>
-    <div class="card-footer text-body-secondary">
-    ${fecha}
-    <button class="btn btn-dark">Eliminar</button>
-  </div>
- </div>
-</div>`;
-  listPendiente.innerHTML += card;
-}
-
-// función crear el evento
-formulario.addEventListener("submit", function (event) {
+// Agregar evento para crear una nueva tarea
+newTaskForm.addEventListener("submit", (event) => {
   event.preventDefault();
+  const title = event.target.querySelector('input[name="title"]');
 
-  crearLista();
+  console.log(title);
+  const description = event.target.querySelector(
+    'textarea[name="description"]'
+  ).value;
+  const dueDate = event.target.querySelector('input[name="due-date"]');
+  addTask(title, description, dueDate);
+  event.target.reset();
 });
+
+//
--- js/main.js
+++ js/main.js
@@ -1,123 +1,3 @@
-// const pendienteList = document.querySelector('#pendiente-list');
-// const progresoList = document.querySelector('#progreso-list');
-// const completadoList = document.querySelector('#completado-list');
-
-// function agregarTarea() {
-//   const titulo = document.querySelector('#titulo').value;
-//   const descripcion = document.querySelector('#descripcion').value;
-//   const fecha = document.querySelector('#fecha').value;
-
-//   const tarea = crearTarea(titulo, descripcion, fecha);
-
-//   pendienteList.appendChild(tarea);
-// }
-
-// function crearTarea(titulo, descripcion, fecha) {
-//   const tarea = document.createElement('div');
-//   tarea.classList.add('tarea');
-
-//   const tituloElemento = document.createElement('h2');
-//   tituloElemento.textContent = titulo;
-
-//   const descripcionElemento = document.createElement('p');
-//   descripcionElemento.textContent = descripcion;
-
-//   const fechaElemento = document.createElement('p');
-//   fechaElemento.textContent = fecha;
-
-//   tarea.appendChild(tituloElemento);
-//   tarea.appendChild(descripcionElemento);
-//   tarea.appendChild(fechaElemento);
-
-//   agregarBotonMover(tarea);
-
-//   return tarea;
-// }
-
-// function agregarBotonMover(tarea) {
-//   const moverBoton = document.createElement('button');
-//   moverBoton.textContent = 'Mover';
-//   moverBoton.classList.add('mover-boton');
-
-//   moverBoton.addEventListener('click', () => {
-//     const listaActual = tarea.parentNode;
-//     switch (listaActual) {
-//       case pendienteList:
-//         progresoList.appendChild(tarea);
-//         break;
-//       case progresoList:
-//         completadoList.appendChild(tarea);
-//         break;
-//       case completadoList:
-//         pendienteList.appendChild(tarea);
-//         break;
-//     }
-//   });
-
-//   tarea.appendChild(moverBoton);
-// }
-
-// const agregarBoton = document.querySelector('#agregar-boton');
-// agregarBoton.addEventListener('click', agregarTarea);
-
-// const editarBotones = document.querySelectorAll('.editar-boton');
-// editarBotones.forEach(boton => {
-//   boton.addEventListener('click', () => {
-//     const tarea = boton.parentNode;
-//     const titulo = tarea.querySelector('h2').textContent;
-//     const descripcion = tarea.querySelector('p').textContent;
-//     const fecha = tarea.querySelectorAll('p')[1].textContent;
-
-//     // Agregar formulario para editar tarea
-//     const formulario = document.createElement('form');
-
-//     const tituloInput = document.createElement('input');
-//     tituloInput.setAttribute('type', 'text');
-//     tituloInput.setAttribute('placeholder', 'Título');
-//     tituloInput.setAttribute('value', titulo);
-
-//     const descripcionInput = document.createElement('textarea');
-//     descripcionInput.setAttribute('placeholder', 'Descripción');
-//     descripcionInput.textContent = descripcion;
-
-//     const fechaInput = document.createElement('input');
-//     fechaInput.setAttribute('type', 'date');
-//     fechaInput.setAttribute('value', fecha);
-
-//     const guardarBoton = document.createElement('button');
-//     guardarBoton.textContent = 'Guardar';
-//     guardarBoton.classList.add('guardar-boton');
-
-//     guardarBoton.addEventListener('click', () => {
-//       const nuevoTitulo = tituloInput.value;
-//       const nuevaDescripcion = descripcionInput.value;
-//       const nuevaFecha = fechaInput.value;
-
-//       tarea.querySelector('h2').textContent = nuevoTitulo;
-//       tarea.querySelector('p').textContent = nuevaDescripcion;
-//       tarea.querySelectorAll('p')[1].textContent = nuevaFecha;
-
-//       tarea.removeChild(formulario);
-//     });
-
-//     const cancelarBoton = document.createElement('button');
-//     cancelarBoton.textContent = 'Cancelar';
-//     cancelarBoton.classList.add('cancelar-boton');
-
-//     cancelarBoton.addEventListener('click', () => {
-//       tarea.removeChild(formulario);
-//     });
-
-//     formulario.appendChild(tituloInput);
-//     formulario.appendChild(descripcionInput);
-//     formulario.appendChild(fechaInput);
-//     formulario.appendChild(guardarBoton);
-//     formulario.appendChild(cancelarBoton);
-
-//     tarea.appendChild(formulario);
-//   });
-// });
-
 const formularioTarea = document.querySelector("#formulario-tarea");
 const inputTitulo = document.querySelector("#titulo");
 const inputDescripcion = document.querySelector("#descripcion");
@@ -138,7 +18,6 @@ function crearTarea(titulo, descripcion, fecha) {
   };
   tareas.push(tarea);
   mostrarTareas();
-  console.log(tarea);
 }
 // crearTarea();
 // Función para mostrar las tareas en las diferentes listas
@@ -148,7 +27,7 @@ function mostrarTareas() {
   listaCompletadas.innerHTML = "";
   tareas.forEach((tarea) => {
     const itemTarea = document.createElement("li");
-    console.log(itemTarea);
+
     itemTarea.innerHTML = `
         <strong>${tarea.titulo}</strong>
         <p>${tarea.descripcion}</p>
@@ -161,7 +40,6 @@ function mostrarTareas() {
       console.log(itemTarea);
       e.dataTransfer.setData("text/plain", tarea.id);
       e.currentTarget.classList.add("dragging");
-      console.log("hola mundo");
     });
     itemTarea.addEventListener("dragend", (e) => {
       e.currentTarget.classList.remove("dragging");
@@ -182,10 +60,15 @@ function mostrarTareas() {
 // console.log(mostrarTareas());
 
 function moverTarea(id, lista) {
-  let tarea = tareas.find((t) => t.id === id);
+  const index = tareas.findIndex((t) => t.id === id);
+  if (index !== -1) {
+    tareas[index].lista = lista;
+    mostrarTareas();
+  }
+  // let tarea = tareas.find((t) => t.id === id);
 
-  tarea.lista = lista;
-  mostrarTareas();
+  // tarea.lista = lista;
+  // mostrarTareas();
 }
 // Evento para enviar el formulario y agregar una nueva tarea
 formularioTarea.addEventListener("submit", (e) => {
